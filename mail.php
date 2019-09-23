e35d256bf7c81f7e6484482a1cdef46f6be08455
Trish <trishbellardine@gmail.com>
2019-09-23 08:54:50 -0700

    all pages

--- mail.php
+++ mail.php
@@ -3,21 +3,21 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "USEREMAIL@gmail.com";
-        
+        $mail_to = "support@trudesigns.co";
+
         # Sender Data
         // $subject = trim($_POST["subject"]);
         $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
-        
+
         if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
             exit;
         }
-        
+
         # Mail Content
         $content = "Name: $name\n";
         $content .= "Email: $email\n\n";
